b7a9ab0d9d5007c511e54f797cd1ce862feb5e72
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-17 19:45:59 +0000

    modified addNote function to check duplicate note title

--- notes.js
+++ notes.js
@@ -6,13 +6,21 @@ const getNotes = function() {
 
 const addNote = function(title, body) {
   const notes = loadNotes();
-
-  notes.push({
-    title: title,
-    body: body
+  const duplicateNotes = notes.filter(function(note) {
+    return note.title === title;
   });
 
-  saveNotes(notes);
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+
+    saveNotes(notes);
+    console.log('New note added.');
+  } else {
+    console.log('Note title is taken.');
+  }
 };
 
 const saveNotes = function(notes) {
