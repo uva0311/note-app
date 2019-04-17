89bca86be6cdfc0e75c6a51908f546696680e178
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-17 19:32:31 +0000

    add addNote function

--- notes.js
+++ notes.js
@@ -1,5 +1,31 @@
+const fs = require('fs');
+
 const getNotes = function() {
   return 'Your notes...';
 };
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+  const notes = loadNotes();
+
+  notes.push({
+    title: title,
+    body: body
+  });
+
+  console.log(notes);
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
