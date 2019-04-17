7680df367e915ee4eb7200a418abdcd8f11ab700
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-17 20:54:53 +0000

    modified with ES6 function definition

--- notes.js
+++ notes.js
@@ -1,15 +1,13 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-const getNotes = function() {
+const getNotes = () => {
   return 'Your notes...';
 };
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function(note) {
-    return note.title === title;
-  });
+  const duplicateNotes = notes.filter(note => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push({
@@ -24,11 +22,9 @@ const addNote = function(title, body) {
   }
 };
 
-const removeNote = function(title) {
+const removeNote = title => {
   const notes = loadNotes();
-  const filteredNote = notes.filter(function(note) {
-    return note.title !== title;
-  });
+  const filteredNote = notes.filter(note => note.title !== title);
 
   if (filteredNote.length < notes.length) {
     console.log(chalk.green.inverse('Note removed'));
@@ -38,12 +34,12 @@ const removeNote = function(title) {
   }
 };
 
-const saveNotes = function(notes) {
+const saveNotes = notes => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
 };
 
-const loadNotes = function() {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync('notes.json');
     const dataJSON = dataBuffer.toString();
