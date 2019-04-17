fe937f372b20f71efca39485e28b02ec1d7edab3
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-17 17:20:42 +0000

    modified add command to include note title and body

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ const getNotes = require('./notes.js');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function() {
-    console.log('Adding a new note!');
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ', argv.title);
+    console.log('Body: ', argv.body);
   }
 });
 
@@ -40,4 +53,4 @@ yargs.command({
   }
 });
 
-console.log(yargs.argv);
+yargs.parse();
