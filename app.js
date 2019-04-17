0c15f012fbef93365cc7102a22eea9763da3eb3e
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-17 19:32:56 +0000

    modified to add note dynamically

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // add, remove, read, list
 
@@ -21,8 +21,7 @@ yargs.command({
     }
   },
   handler: function(argv) {
-    console.log('Title: ', argv.title);
-    console.log('Body: ', argv.body);
+    notes.addNote(argv.title, argv.body);
   }
 });
 
